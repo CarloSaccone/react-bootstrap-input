7831666b66c2e69510d82715a1dd60f74edc9f41
carl saccone <carlosaccone@yahoo.it>
2019-11-28 12:19:18 +0100

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# react-bootstrap-input
+
+This package provides a simple yet powerful input component with the following features:
+
+-   pattern-based validation
+-   form-group validation
+-   data-binding
+-   input-masks and formatting
+
+## basics
+
+Create a host component representing your form (a filter form in this example), then place in the render function a few input fields:
+
+```
+<SimpleInput
+    formObj={filter}
+    name="email"
+    placeholder="Your email"
+    onChange={filterChange}
+    required
+    validated
+    pattern={'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$'}
+    errorMessage="Please provide a valid email address"
+/>
+```
+
+Provide the data-variable representing your form and the change function:
+
+```
+const [filter, setfilter] = useState({});
+const filterChange = updatedItem => {
+    setfilter(updatedItem);
+};
+```
+
+Enjoy!
+![demo](./public/react-bootstrap-input.gif)
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+/*** webpack.config.js ***/
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, 'examples/src/index.html'),
+    filename: './index.html'
+});
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+    entry: path.join(__dirname, 'examples/src/index.js'),
+    output: {
+        path: path.join(__dirname, 'examples/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [
+                    isDevelopment
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                exclude: /\.module.(s(a|c)ss)$/,
+                loader: [
+                    isDevelopment
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        htmlWebpackPlugin,
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    },
+    devServer: {
+        port: 3001
+    }
+};
