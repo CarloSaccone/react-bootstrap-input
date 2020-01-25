228764b47af54eb2fce8b912cfb9f5f2472b04eb
Marco Allotta <marcoallott@gmail.com>
2020-01-25 13:16:47 +0100

    fix keys

--- examples/allottaFor200crowd/src/List.js
+++ examples/allottaFor200crowd/src/List.js
@@ -1,9 +1,9 @@
 import React from "react";
 import isEmpty from "lodash/isEmpty";
 const List = ({ items = [] }) => {
-  const children = items.map(item => {
+  const children = items.map((item, index) => {
     return (
-      <tr key={item.name}>
+      <tr key={item.name + index}>
         <td>{item.name}</td>
         <td>{item.email}</td>
         <td>{item.salary}</td>
